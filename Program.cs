fdc270bef8ce31a4301eca70e8c64b110f88f927
bhlokana@microsoft.com <bhlokana@microsoft.com>
2024-03-21 10:10:30 -0500

    init

---
+++ Program.cs
@@ -0,0 +1,51 @@
+using Azure.Identity;
+using Microsoft.Azure.Cosmos;
+using ContosoSuitesWebAPI.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<ICosmosService, CosmosService>();
+
+builder.Services.AddSingleton<CosmosClient>((_) =>
+{
+    CosmosClient client = new(
+        connectionString: builder.Configuration["AZURE_COSMOS_DB_CONNECTION_STRING"]!
+    );
+    return client;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/Customer", async (string searchCriterion, string searchValue) => 
+{
+    switch (searchCriterion)
+    {
+        case "CustomerName":
+            return await app.Services.GetService<ICosmosService>()!.GetCustomersByName(searchValue);
+        case "LoyaltyTier":
+            return await app.Services.GetService<ICosmosService>()!.GetCustomersByLoyaltyTier(searchValue);
+        case "DateOfMostRecentStay":
+            return await app.Services.GetService<ICosmosService>()!.GetCustomersWithStaysAfterDate(DateTime.Parse(searchValue));
+        default:
+            throw new Exception("Invalid search criterion. Valid search criteria include 'CustomerName', 'LoyaltyTier', and 'DateOfMostRecentStay'.");
+    }
+})
+
+    .WithName("GetCustomer")
+    .WithOpenApi();
+
+app.Run();
---
+++ Services/CosmosService.cs
@@ -0,0 +1,64 @@
+using System.Runtime.CompilerServices;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
+
+namespace ContosoSuitesWebAPI.Services;
+
+public class CosmosService : ICosmosService
+{
+    private readonly CosmosClient _client;
+    private Container container
+    {
+        get => _client.GetDatabase("contososuites").GetContainer("Customers");
+    }
+
+    public CosmosService()
+    {
+        _client = new CosmosClient(
+            connectionString: Environment.GetEnvironmentVariable("AZURE_COSMOS_DB_CONNECTION_STRING")!
+        );
+    }
+
+    public async Task<IEnumerable<Customer>> GetCustomersByName(string name)
+    {
+        var queryable = container.GetItemLinqQueryable<Customer>();
+        using FeedIterator<Customer> feed = queryable
+            .Where(c => c.FullName == name)
+            .ToFeedIterator<Customer>();
+        return await ExecuteQuery(feed);
+    }
+
+    public async Task<IEnumerable<Customer>> GetCustomersByLoyaltyTier(string loyaltyTier)
+    {
+        LoyaltyTier lt = Enum.Parse<LoyaltyTier>(loyaltyTier);
+        var queryable = container.GetItemLinqQueryable<Customer>();
+        using FeedIterator<Customer> feed = queryable
+            .Where(c => c.LoyaltyTier.ToString() == loyaltyTier)
+            .ToFeedIterator<Customer>();
+        return await ExecuteQuery(feed);
+    }
+
+    public async Task<IEnumerable<Customer>> GetCustomersWithStaysAfterDate(DateTime dt)
+    {
+        var queryable = container.GetItemLinqQueryable<Customer>();
+        using FeedIterator<Customer> feed = queryable
+            .Where(c => c.DateOfMostRecentStay > dt)
+            .ToFeedIterator<Customer>();
+        return await ExecuteQuery(feed);
+    }
+
+    private async Task<IEnumerable<Customer>> ExecuteQuery(FeedIterator<Customer> feed)
+    {
+        List<Customer> results = new();
+        while (feed.HasMoreResults)
+        {
+            var response = await feed.ReadNextAsync();
+            foreach (Customer c in response)
+            {
+                results.Add(c);
+            }
+        }
+        return results;
+    }
+
+}
