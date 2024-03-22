9a30166c3729e38b727bbeb1c267158f4ae547be
bhlokana@microsoft.com <bhlokana@microsoft.com>
2024-03-21 20:57:03 -0500

    add function url

--- obj/Debug/net8.0/ContosoSuitesWebAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContosoSuitesWebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoSuitesWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c80e2c05053a9655ce42c71f226618091aa03e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62d2df452fa84c1333b63112eed3ca5a7e1c02fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoSuitesWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoSuitesWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
